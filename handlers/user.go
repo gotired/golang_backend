0bc83ffaab82a8b12c9f9183a730d55dbc19f2e5
gotired <gotired@hotmail.com>
2024-03-28 23:56:11 +0700

    feat: change output to use response schema

--- handlers/user.go
+++ handlers/user.go
@@ -2,13 +2,18 @@ package handlers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gotired/golang_backend/models"
 	service "github.com/gotired/golang_backend/services"
 )
 
 func ListUsers(c *fiber.Ctx) error {
 	users, err := service.GetAllUsers()
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			Failure{}.Detail(err.Error(), "handlers/user/ListUsers"))
 	}
-	return c.JSON(users)
+	if users == nil {
+		users = make([]models.User, 0)
+	}
+	return c.JSON(Success{}.Data(users))
 }
