9763a5199305209f06e8c0f877ffc1b3a9475355
gotired <gotired@hotmail.com>
2024-04-03 23:59:11 +0700

    feat: use gorm instead of built in sql

--- services/user.go
+++ services/user.go
@@ -5,24 +5,12 @@ import (
 	"github.com/gotired/golang_backend/models"
 )
 
-func GetAllUsers() ([]models.User, error) {
-	var users []models.User
+func GetAllUsers() ([]models.UserDetail, error) {
+	var users []models.UserDetail
 	db := database.GetDB()
 
-	rows, err := db.Query("SELECT id, name, email FROM users")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var user models.User
-		if err := rows.Scan(&user.ID, &user.Name, &user.Email); err != nil {
-			return nil, err
-		}
-		users = append(users, user)
-	}
-	if err := rows.Err(); err != nil {
+	if err := db.Table("users").Select("users.*").
+		Find(&users).Error; err != nil {
 		return nil, err
 	}
 
