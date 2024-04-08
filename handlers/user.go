314c3c20bd16aed3426b6f9b7d740089f886ccc9
gotired <gotired@hotmail.com>
2024-04-09 00:09:24 +0700

    refactor: register function

--- handlers/user.go
+++ handlers/user.go
@@ -1,28 +1,18 @@
 package handlers
 
 import (
-	"errors"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
 	"github.com/gotired/golang_backend/database"
 	"github.com/gotired/golang_backend/models"
-	service "github.com/gotired/golang_backend/services"
+	user_credential_service "github.com/gotired/golang_backend/services/user_credential"
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 )
-
-// type User interface {
-// 	List(c *fiber.Ctx) error
-// 	Register(c *fiber.Ctx) error
-// 	Login(c *fiber.Ctx) error
-// }
 
 type UserStruct struct {
 }
 
 func (u *UserStruct) List(c *fiber.Ctx) error {
-	users, err := service.GetAllUsers()
+	users, err := services.GetAllUsers()
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(
 			Failure{}.Detail(err.Error(), "handlers/user/List"))
@@ -44,29 +34,12 @@ func (u *UserStruct) Register(c *fiber.Ctx) error {
 			Failure{}.Detail("Mismatch Password and Confirm password", "handlers/user/Register"))
 	}
 
-	db := database.GetDB()
-
-	var existingUser models.User
-	if err := db.Where("email = ?", requestBody.Email).First(&existingUser).Error; err == nil {
-		return c.Status(fiber.StatusBadRequest).JSON(
-			Failure{}.Detail("Email is already registered", "handlers/user/Register"))
-	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
-		// If an unexpected error occurred during the query, return it
-		return err
-	}
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(requestBody.Password), bcrypt.DefaultCost)
+	user_id, err := user_credential_service.Register(requestBody.Email, requestBody.Username, requestBody.Username, requestBody.Password)
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			Failure{}.Detail(err.Error(), "handlers/user/Register"))
 	}
-	userID := uuid.New()
-
-	user := models.User{ID: userID.String(), Email: requestBody.Email, Password: string(hashedPassword), FirstName: requestBody.FirstName, LastName: requestBody.LastName, Phone: requestBody.Phone, Username: requestBody.Username}
-	if err := db.Create(&user).Error; err != nil {
-		return err
-	}
-
-	return c.JSON(Success{}.Detail("User registered successfully!"))
+	return c.JSON(Success{}.Detail("User registered successfully! " + (*user_id).String()))
 }
 
 func (u *UserStruct) Login(c *fiber.Ctx) error {
