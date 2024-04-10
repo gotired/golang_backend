8c3fc61c0920bd8081b3076c42e00cc3e17ae3fe
gotired <gotired@hotmail.com>
2024-04-10 20:36:19 +0700

    feat: add jwt secret key to config model

--- models/config.go
+++ models/config.go
@@ -1,10 +1,11 @@
 package models
 
 type Config struct {
-	DBUsername string
-	DBPassword string
-	DBHost     string
-	DBPort     int
-	DBName     string
-	APPPort    int
+	DBUsername   string
+	DBPassword   string
+	DBHost       string
+	DBPort       int
+	DBName       string
+	APPPort      int
+	JWTSecretKey string
 }
