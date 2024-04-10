95a7865026042e2f681a4f147aac7f3953a895a9
gotired <gotired@hotmail.com>
2024-04-10 20:37:22 +0700

    refactor: rename get user info function

--- services/user_info/get.go
+++ services/user_info/get.go
@@ -2,14 +2,14 @@ package services
 
 import (
 	"github.com/gotired/golang_backend/database"
-	"github.com/gotired/golang_backend/models"
+	"github.com/gotired/golang_backend/models/table"
 )
 
-func GetAllUsers() ([]models.UserDetail, error) {
-	var users []models.UserDetail
+func Get() ([]table.UserInfo, error) {
+	var users []table.UserInfo
 	db := database.GetDB()
 
-	if err := db.Table("users").Select("users.*").
+	if err := db.Table("user_infos").Select("*").
 		Find(&users).Error; err != nil {
 		return nil, err
 	}
